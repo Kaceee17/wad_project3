ed0ca951b03c959d206dc25f73e8943063f1f527
Kaceee17 <146193981+Kaceee17@users.noreply.github.com>
2023-11-02 18:39:42 +0700

    Add files via upload

---
+++ wad_project3.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Nov 2023 pada 12.37
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wad_project3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(10) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `nim` int(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `student`
+--
+
+INSERT INTO `student` (`id`, `name`, `nim`, `address`) VALUES
+(1, 'Azka Chanda M', 1202213278, 'Jl. Mengger Hilir no.1'),
+(2, 'Billy Orlando S', 1202213279, 'Jl. Telekomunikasi no.2');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
