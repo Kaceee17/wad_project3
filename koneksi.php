09584b7584772d36f5f51ace60a2688f52d89510
Kaceee17 <146193981+Kaceee17@users.noreply.github.com>
2023-11-02 18:38:55 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect ("localhost","root", "", "WAD_PROJECT3");
-$result = mysqli_query ($conn,"SELECT * FROM student");
-// var_dump($result);
-var_dump(mysqli_fetch_object($result));
-?>
